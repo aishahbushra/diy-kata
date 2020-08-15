3b2c4878405be786658d0145508babaae434bc1a
Aishah Bushra <aishahbushra@outlook.com>
2020-08-15 20:03:19 +0100

    reachDestination done and DONE

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,14 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+let timeTaken = distance / speed;
+
+let roundedInt = Math.round(timeTaken * 2)
+
+let roundedTime = roundedInt / 2;
+
+console.log("I should be there in " + roundedTime + " hours");
+
+  return "I should be there in " + roundedTime + " hours";
+};
+
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination((distance = 44), (speed = 10))).toBe(
+      "I should be there in 4.5 hours"
+    );
+  });
 });
