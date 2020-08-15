620aedaf750d2264824c3af8477d8c39e1d2a0b8
Aishah Bushra <aishahbushra@outlook.com>
2020-08-15 22:24:14 +0100

    joinNames sorted

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,22 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  let first = namesObj[0];
+  let second = namesObj[1];
+  let third = namesObj[2];
+  const arrayOfNames = []
+  arrayOfNames.push(first.name, second.name, third.name);
+  arrayOfNames.join(', ');
+
+  const finalArray = arrayOfNames.slice(0, -1).join(', ')+' & '+arrayOfNames.slice(-1);
+
+  return finalArray;
+
+//    //arrayOfNames.reduce((previous, current, index, array) => {
+//     if (i === array.length - 1) {
+//       return previous + " & " + current;
+//     } 
+//     return previous + ", " + current;
+//   })
+//   return arrayOfNames; //
+};
 
 module.exports = joinNames;
