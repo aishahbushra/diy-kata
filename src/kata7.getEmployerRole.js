bf4c7ddbf633e76255ba9336c0d52d1b9f41586e
Aishah Bushra <aishahbushra@outlook.com>
2020-08-16 17:52:02 +0100

    employerRole done

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,11 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  for (i = 0; i < employees.length; i++){
+    if (employees[i].name === employeeName) {
+      return employees[i].role;
+    }
+  }
+
+};
+
 
 module.exports = getEmployerRole;
